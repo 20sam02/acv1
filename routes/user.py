d15892c4a8425aae7e405b870972d405512e83cd
samir quintana <saqv2002@gmail.com>
2025-02-21 10:30:45 -0500

    nuevas carpetas pero es crud no está completo

--- routes/user.py
+++ routes/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from config.db import conn
 from models.user import users
+from schemas.user import User
 
 user = APIRouter()
 
@@ -9,9 +10,10 @@ user = APIRouter()
 def get_users():
         return conn.execute(users.select()).fetchall()
 
-@user.get("/users")
+@user.post("/users")
 
-def helloworld():
+def create_user(user:User):
+        print(User)
         return "hello world 2"
 
 @user.get("/users")
